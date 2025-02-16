5f048fe7ac1fee64eb14e74cea5c04ba23eb7135
Abdel Maoula <88989374+abdelmaoulagr@users.noreply.github.com>
2025-02-16 13:41:24 -0500

    first commit

---
+++ dags/reddit_dag.py
@@ -0,0 +1,33 @@
+import airflow from DAG 
+import datetime from datetime
+import os 
+import sys 
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+default_args={
+    'owner':'Abdel Maoula',
+    'start_date':datetime(2025,2,16)
+}
+
+file_postfix= datetime.now().strftime("%Y%m%d")
+
+dag= DAG(
+    dag_id='etl_reddit_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit','etl','pipeline']
+)
+
+#extraction from reddit
+extract= PythonOperator(
+    task_id = 'reddit_extraction',
+    python_callable=reddit_pipeline,
+    op_kwargs={
+        'file_name':f'reddit_{file_postfix}',
+        'subreddit':'dataengineering',
+        'time_fiter':'day',
+        'limit':100
+    }
+)
---
+++ pipelines/reddit_pipeline.py
@@ -0,0 +1,2 @@
+def reddit_pipeline(file_name:str, subreddit:str, time_fiter:'day', limit=None):
+    
