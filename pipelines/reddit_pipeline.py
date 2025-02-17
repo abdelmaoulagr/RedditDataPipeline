71a35be2818a93be2140ae8e6116953e90b0d90e
Abdel Maoula <88989374+abdelmaoulagr@users.noreply.github.com>
2025-02-17 13:36:16 -0500

    install configparser

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,5 +1,6 @@
 def reddit_pipeline(file_name:str, subreddit:str, time_fiter:'day', limit=None):
     #connecting to reddit instance
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airsholar Agent')
     #extraction
     #transformation
     #loading to csv
