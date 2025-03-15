0417fc6e993218f9a07701977c7cbc8cb64c5ec9
Abdel Maoula <88989374+abdelmaoulagr@users.noreply.github.com>
2025-03-14 23:42:35 -0400

    s3 connection fixed

--- etls/aws_etl.py
+++ etls/aws_etl.py
@@ -3,7 +3,7 @@ from utils.constants import AWS_ACCESS_KEY_ID,AWS_ACCESS_KEY
 
 def connect_to_s3():
     try:
-        s3= s3sf.S3FileSystem(anon = False,
+        s3= s3fs.S3FileSystem(anon = False,
                                 key= AWS_ACCESS_KEY_ID,
                                 secret=AWS_ACCESS_KEY)
         return s3
